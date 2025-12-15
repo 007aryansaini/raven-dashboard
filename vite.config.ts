f8d8c5c9a8cecac03462dbe68fa719152879d74c
007aryansaini <aryansaini0007@gmail.com>
2025-12-15 16:20:31 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const SCORE_API_HOST = 'http://provider.h100.hou.sfc.akash.pub:30640'
-const CHAT_API_HOST = 'http://vns88n43rlb0vd82ntt2j7nngg.ingress.h100.hou.sfc.akash.pub'
+const SCORE_API_HOST = 'http://provider.h100.ams.val.akash.pub:30288'
+const CHAT_API_HOST = 'http://keq230rf0lc27ej8leqppaqe04.ingress.h100.ams.val.akash.pub'
 const BACKEND_HOST = 'https://agent-asva-temp.vercel.app'
 
 // https://vite.dev/config/
